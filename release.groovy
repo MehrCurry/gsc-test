2cac7d73df05db8eea450e36ab1f78650ad4bfe5
Guido Zockoll <gzockoll@gmail.com>
2013-07-20 07:15:19 +0200

    release plugin version 2.3.2

---
+++ release.groovy
@@ -0,0 +1,10 @@
+@Grab(group='com.atlassian.jgitflow', module='jgit-flow', version='0.10')
+import com.atlassian.jgitflow.core.*
+
+//get or initialize an instance using the default branch names / prefixes
+JGitFlow flow = JGitFlow.getOrInit(new File("."))
+
+//start a feature with a fetch
+flow.featureStart("my-feature").setFetchDevelop(true).call()
+
+println "Success"
