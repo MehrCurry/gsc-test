29f624471e71cc87959e37fda26e0316d1679d70
Guido Zockoll <gzockoll@gmail.com>
2013-07-20 07:18:30 +0200

    release plugin version 2.3.2

--- release.groovy
+++ release.groovy
@@ -1,5 +1,12 @@
-@Grab(group='com.atlassian.jgitflow', module='jgit-flow', version='0.10')
+@Grapes(
+        @Grab(group = 'com.atlassian.jgitflow', module = 'jgit-flow', version = '0.10')
+)
 import com.atlassian.jgitflow.core.*
+
+@Grapes(
+        @Grab(group = 'com.atlassian.jgitflow', module = 'jgit-flow', version = '0.10')
+)
+import java.lang.Object
 
 //get or initialize an instance using the default branch names / prefixes
 JGitFlow flow = JGitFlow.getOrInit(new File("."))
